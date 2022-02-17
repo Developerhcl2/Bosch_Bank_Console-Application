833f98444b4a6c079e401b2eaa52172792b44947
Developerhcl2 <darakshan.ahmer@hcl.com>
2022-02-17 05:27:56 +0000

    Commit First

---
+++ Polymorphism_EX/Basic_class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_EX
+{
+    class Basic_class
+    {
+        public static void Sum(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public static void Sum(int x, int y,int z)
+        {
+            Console.WriteLine(x + y +z);
+        }
+        public static void Sum(int x)
+        {
+            Console.WriteLine(x++);
+        }
+        public static void Sum(int x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
---
+++ Polymorphism_EX/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Polymorphism_EX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Basic_class.Sum(10, 12);
+            Basic_class.Sum(10, 12,20);
+            Basic_class.Sum(10);
+            Basic_class.Sum(10, 12.05);
+            
+
+        }
+    }
+}
